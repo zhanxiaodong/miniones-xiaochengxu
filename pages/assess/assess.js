4ce8c0541d81a5f0f88a5b67fef6e44294d1411d
zxd <103258469@qq.com>
2018-10-22 14:55:52 +0800

    zxd

--- pages/assess/assess.js
+++ pages/assess/assess.js
@@ -4,7 +4,7 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
+  /*data: {
     iconfontList:[
       { icon:"icon-xingxing"},
       { icon: "icon-xingxing" },
@@ -14,61 +14,97 @@ Page({
 
     ]
   
+  },*/
+
+  data: {
+    icon: '/images/star.png',
+    iconGreen: '/images/star-green.png',
+    starFlag: null,
+    selectedFavorites: [],
+    starList: [
+      {
+        favorites: [
+          { value: 1, name: '操作麻烦' },
+          { value: 2, name: '服务不够' },
+          { value: 3, name: '价格太高' },
+          { value: 4, name: '搭配不力' },
+          { value: 5, name: '品质不佳' },
+          { value: 6, name: '没有惊喜' },
+        ]
+      },
+      {
+        favorites: [
+          { value: 1, name: '操作麻烦' },
+          { value: 2, name: '服务不够' },
+          { value: 3, name: '价格太高' },
+          { value: 4, name: '搭配不力' },
+          { value: 5, name: '品质不佳' },
+          { value: 6, name: '没有惊喜' },
+        ]
+      },
+      {
+        favorites: [
+          { value: 1, name: '操作麻烦' },
+          { value: 2, name: '服务不够' },
+          { value: 3, name: '价格太高' },
+          { value: 4, name: '搭配不力' },
+          { value: 5, name: '品质不佳' },
+          { value: 6, name: '没有惊喜' },
+        ]
+      },
+      {
+        favorites: [
+          { value: 1, name: '比较省心' },
+          { value: 2, name: '服务还行' },
+          { value: 3, name: '价格还行' },
+          { value: 4, name: '搭配还行' },
+          { value: 5, name: '品质还行' },
+          { value: 6, name: '较有惊喜' },
+        ]
+      },
+      {
+        favorites: [
+          { value: 1, name: '省心' },
+          { value: 2, name: '贴心' },
+          { value: 3, name: '实惠' },
+          { value: 4, name: '没搭' },
+          { value: 5, name: '品质' },
+          { value: 6, name: '惊喜' },
+        
+        ]
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onSelectFavorites(e) {
+    // 收集数据
+    this.data.selectedFavorites =  e.detail.value
+    // ui 显示逻辑
+    var index = e.target.dataset.index1
+    var star = this.data.starList[index]
+    var favorites = star.favorites
+    for (let i = 0; i < favorites.length; i++ ) {
+      let favor = favorites[i];
+      if (e.detail.value.includes(String(favor.value)) ) {
+        favorites[i].checked = true
+      } else {
+        favorites[i].checked = false
+      }
+    }
+    this.setData({
+      ['starList[' + index + ']']: star
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onSelect(e) {
+    var index = e.target.dataset.index
+    this.setData({
+      starFlag: Number(index)
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+  doPageToNext() {
+    console.log('selectedFavorites', this.data.selectedFavorites)
+    console.log('selectedFavorites', this.data.selectedFavorites.join(','))
+  }  
 })
