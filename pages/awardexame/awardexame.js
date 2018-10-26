eadf809e84c140a625e3a8fa969bc23dd1d2e228
zxd <103258469@qq.com>
2018-10-26 17:15:19 +0800

    zxd

--- pages/awardexame/awardexame.js
+++ pages/awardexame/awardexame.js
@@ -6,6 +6,8 @@ Page({
    */
   data: {
      awardImg: '/images/bd1.png',
+     deadline: '2018-10-31 00:00截至',
+     count: '998',
      awardList: [{
        index: '奖项一',
        detail: '仔细答题结束可以获得随机现金红包'
@@ -16,59 +18,9 @@ Page({
      }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  clickTab: function () {
+    wx.navigateTo({
+      url: '../awardone/awardone',
+    })
   }
 })
