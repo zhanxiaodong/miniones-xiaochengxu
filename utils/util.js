e890bdde7b847656a3ab76f0ad0e7a365d7e2d7d
zxd <103258469@qq.com>
2018-12-20 10:47:21 +0800

    zxd

--- utils/util.js
+++ utils/util.js
@@ -434,8 +434,8 @@ function radioGroupChange(group, value) {
 }
 
 // const requestUrl = 'http://interface.miniones.cn/merchant/'
-// const requestUrl = 'https://interface.miniones.cn/merchant_new/'
- const requestUrl = 'http://192.168.0.22:8080/merchant/'
+const requestUrl = 'https://interface.miniones.cn/merchant_new/'
+ //const requestUrl = 'http://192.168.0.22:8080/merchant/'
 // const requestUrl = 'http://localhost:8080/merchant/'
 function imageUtil(e, windowWidth, windowHeight) {
   var imageSize = {};
