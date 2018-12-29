6c09e2661833fa01a713f3b55201d886239ee9b9
zxd <103258469@qq.com>
2018-12-29 11:32:08 +0800

    zxd

---
+++ pages/peoplehead/peoplehead.js
@@ -0,0 +1,20 @@
+// pages/peoplehead/peoplehead.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgs: [
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/topbackground.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu7.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu8.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu9.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu10.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu7.png?x-oss-process=image/resize,h_222,w_355',
+      'https://miniany.oss-cn-beijing.aliyuncs.com/minianys/lunbotu8.png?x-oss-process=image/resize,h_222,w_355',
+    ]
+  },
+
+  
+})
